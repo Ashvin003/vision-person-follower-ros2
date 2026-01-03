e9ba0a1ef6c4370bbe4a2595e4f9a443728266c7
Ashvin003 <ashvin.dandothkar018@gmail.com>
2026-01-03 23:51:10 +0100

    Cleanup: keep source code only, remove build artifacts

--- vision_person_follower_ws/build/vision_person_detector/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ashvin/vision_person_follower_project/vision_person_follower_ws/install/vision_person_detector'
